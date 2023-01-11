10c248f1c589a380364514921fdce98b189c67cb
John Leamy <jwleamy@gmail.com>
2023-01-10 16:21:58 -0800

    developed initial prompt

--- index.js
+++ index.js
@@ -8,4 +8,42 @@ const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 
 
-// ---- Primary Function to Obtain user information -----
+// ---- Primary Function that prompts users with questions to obtain team member information -----
+function getMembers() {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          message: "Please classify the team member you would like to add?",
+          name: 'role',
+          choices: ["Manager", "Engineer", "Intern"],
+          validate: (value) => { if (value) { return true } else { return "Please choose an option to proceeed" } },
+  
+        },
+        {
+          type: "input",
+          name: "name",
+          message: `What is the team member's name? `,
+          validate: (value) => { if (value) { return true } else { return "Please input a name to proceeed" } },
+  
+        },
+        {
+          type: 'input',
+          message: `What is the team member's id?`,
+          name: 'id',
+          validate: (value) => { if (value) { return true } else { return "Please input an ID to proceed" } },
+        },
+        {
+          type: 'input',
+          message: "What is the team member's email?",
+          name: "email",
+          validate: (value) => { if (value) { return true } else { return "Please input an email to proceed" } },
+  
+        }
+    ])
+}
+
+// ---- Function to Declare what type of additonial information needed (based of the type of team member chosen in the prompt) -----
+findinfo = () => {
+    
+}
