99a31bf9afe1a4cf8e85a02dd2b667a2ed6bb8c9
John Leamy <jwleamy@gmail.com>
2023-01-10 19:23:52 -0800

    added inserthtml function

--- index.js
+++ index.js
@@ -54,32 +54,10 @@ const inserthtml = (constructor) => {
         const email = constructor.getEmail();
         const officeNumber = constructor.getOfficeNumber();
         const school = constructor.getSchool();
-        const GitHub = constructor.getGithub()
-        let content = `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-                integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-                integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-                crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
-                crossorigin="anonymous"></script>
-            <link rel="stylesheet" href="style.css">
-            <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-            <title>My Team Generator</title>
-        </head>
-        <body>
-            <h1 class="title">My Team</h1>
-            <div class="container">
-                <div class="row row-cols-1 row-cols-md-3">`;
-
+        const GitHub = constructor.getGithub();
+        let content = ``;
         if (role = "Intern") {
-            let infobox = `
+            content = `
             <div class="col mb-4">
                 <div class="card h-100">   
                     <header>
@@ -93,10 +71,9 @@ const inserthtml = (constructor) => {
                     </ul>
                 </div>
             </div>`;
-        content.append(infobox);
         }
         else if (role = "Engineer") {
-            let infobox = `
+            content = `
             <div class="col mb-4">
                 <div class="card h-100">
                     <header>
@@ -110,10 +87,9 @@ const inserthtml = (constructor) => {
                     </ul>
                 </div>
             </div>`;
-        content.append(infobox);
         }
         else if (role = "Manager") {
-            let infobox = `
+            content = `
             <div class="col mb-4">
                 <div class="card h-100">
                     <header>
@@ -127,8 +103,8 @@ const inserthtml = (constructor) => {
                     </ul>
                 </div>
             </div>`;
-        content.append(infobox) 
         }
+        fs.appendFileSync("./dist/index.html", content)
     }
     )    
 }
@@ -172,9 +148,12 @@ const addedinfo = ({role, name, id, email}) => {
                 memberinfo()
             }
 
+            inserthtml(memberinformation);
+
         }
 
     )
+
 }
 
 memberinfo()
